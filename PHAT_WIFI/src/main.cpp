0cc8925625fc23bd853322eb85ca7c5d08e03289
schooling000 <schooling010@gmail.com>
2023-01-26 21:25:10 +0700

    update

--- PHAT_WIFI/src/main.cpp
+++ PHAT_WIFI/src/main.cpp
@@ -5,12 +5,12 @@
  * ĐỊNH NGHĨA HẰNG SỐ TÊN WIFI MODULE PHÁT
  *
  */
-const char *NAME_ACCESS_WIFI = "LOCKHANG-1234567890";
+const char *NAME_ACCESS_WIFI = "lockhang1";
 /**
  * ĐỊNH NGHĨA HẰNG SỐ MẬT KHẨU WIFI MODULE PHÁT
  *
  */
-const char *PASS_ACCESS_WIFI = "1234567890";
+const char *PASS_ACCESS_WIFI = "0972001445";
 
 /**
  * KHAI BÁO BIẾN IP MẶT ĐỊNH CHO MODULE ESP8266
@@ -36,6 +36,7 @@ byte wifiStatus = 0;
 void setup()
 {
   Serial.begin(9600);
+  Serial.println();
   Serial.println("-----------------------------------------------------------------------------------");
 
   /**
@@ -68,7 +69,7 @@ void setup()
    * BẬT CHẾ ĐỘ PHÁT WIFI CHO MODULE ESP8266
    * 
    */
-  WiFi.mode(WIFI_AP);
+  WiFi.mode(WIFI_STA);
 
 
 
@@ -78,8 +79,8 @@ void setup()
    */
   Serial.println("-----------------------------------------------------------------------------------");
   Serial.println("CẤU HÌNH MODULE ESP8266 MẶT ĐỊNH");
-  Serial.print("IP MODULE ESP8266: ");Serial.println(WiFi.softAPIP());
-  Serial.print("MACADDRESS MODULE ESP8266: ");Serial.println(WiFi.softAPmacAddress().c_str());
+  Serial.print("IP MODULE ESP8266: ");Serial.println(WiFi.localIP());
+  Serial.print("MACADDRESS MODULE ESP8266: ");Serial.println(WiFi.macAddress().c_str());
   Serial.println("-----------------------------------------------------------------------------------");
 
   /**
@@ -90,20 +91,46 @@ void setup()
    * @param subnet  submet mới cho module
    *
    * @return boolean Trả về giá trị ture nếu thành công false thất bại
-   *    
-   * Serial.println("CẤU HÌNH MODULE ESP8266 SAU KHI CẤU HÌNH LẠI");
-   * Serial.printf("CẤU HÌNH MODULE ESP8266 LẠI %s\n", WiFi.softAPConfig(staticIP, gateway, subnet) ? "THÀNH CÔNG" : "THẤT BẠI");
-   * Serial.print("IP MODULE ESP8266: ");Serial.println(WiFi.softAPIP());
-   * Serial.print("MACADDRESS MODULE ESP8266: ");Serial.println(WiFi.softAPmacAddress().c_str());
-   */
+   */   
+
+  Serial.println("CẤU HÌNH MODULE ESP8266 SAU KHI CẤU HÌNH LẠI");
+  Serial.printf("CẤU HÌNH MODULE ESP8266 LẠI %s\n", WiFi.config(staticIP, gateway, subnet) ? "THÀNH CÔNG" : "THẤT BẠI");
+  Serial.print("IP MODULE ESP8266: ");Serial.println(WiFi.localIP());
+  Serial.print("MACADDRESS MODULE ESP8266: ");Serial.println(WiFi.macAddress().c_str());
 
   /**
-   * KHỞI TẠO ĐIỂM PHÁT WIFI MODULE ESP8266
+   * KẾT NỐI ĐẾN ĐIỂM PHÁT WIFI
    * 
    */
   Serial.println("-----------------------------------------------------------------------------------");
-  Serial.printf("ĐIỂM PHÁT WIFI MODULE ESP8266 KHỞI TẠO %s\n", WiFi.softAP(NAME_ACCESS_WIFI, PASS_ACCESS_WIFI) ? "THÀNH CÔNG" : "THẤT BẠI");
+  Serial.printf("KẾT NỐI ĐẾN ĐIỂM PHÁT WIFI %s\n", WiFi.begin(NAME_ACCESS_WIFI, PASS_ACCESS_WIFI) ? "THÀNH CÔNG" : "THẤT BẠI");
   Serial.println("-----------------------------------------------------------------------------------");
+
+  /**
+   * THÔNG BÁO TRẠNG THÁI CỦA MODULE WIFI (ĐOẠN CODE NÀY THƯỜNG DÙNG TRONG CHẾ ĐỘ STATION)
+   * 
+   */
+  wifiStatus = WiFi.status();
+  switch (wifiStatus)
+  {
+    case WL_CONNECTED:
+      Serial.println("MODULE ESP8266 ĐÃ KẾT NỐI WIFI THÀNH CÔNG");
+      break;
+    case WL_NO_SSID_AVAIL:
+      Serial.println("MODULE ESP8266 KHÔNG TÌM THẤY WIFI");
+      break;
+    case WL_CONNECT_FAILED:
+      Serial.println("MODULE ESP8266 ĐÃ KẾT NỐI THẤT BẠI");
+      break;
+    case WL_WRONG_PASSWORD:
+      Serial.println("MODULE ESP8266 ĐÃ KẾT NỐI THẤT BẠI DO SAI MẬT KHẨU");
+      break;
+    case WL_DISCONNECTED:
+      Serial.println("MODULE ESP8266 ĐÃ NGẮT KẾT NỐI");
+      break;
+    default:
+      Serial.println("MODULE ESP8266 TRONG TRANG THÁI NGHỈ HAY TRẠNG THÁI ĐIỂM TRUY CẬP");
+  }
 
   /**
    * LẤY ĐỊA CHỈ IP TRUY CẬP CỦA MODULE ESP8266
@@ -111,19 +138,10 @@ void setup()
    */
 
   Serial.println("-----------------------------------------------------------------------------------");
-  Serial.print("MODULE ESP8266 NAME \"");Serial.print(WiFi.softAPSSID());Serial.print("\" WIDTH IP: ");Serial.println(WiFi.softAPIP());
+  Serial.print("MODULE ESP8266 NAME \"");Serial.print(WiFi.SSID());Serial.print("\" WIDTH IP: ");Serial.println(WiFi.localIP());
   Serial.println("-----------------------------------------------------------------------------------");
 
-  /**
-   * HÀM LẤY SỐ LƯỢNG STATION KẾT NỐI ĐIẾN ĐIỂM PHÁT WIFI MUDULE ESP8266
-   * MAX STATION KẾT NỐI LÀ 4
-   * 
-   */
-  Serial.println("-----------------------------------------------------------------------------------");
-  Serial.printf("NUM STATION CONNECTED WIDTH MODULE IS: = %d\n", WiFi.softAPgetStationNum());
-  Serial.println("-----------------------------------------------------------------------------------");
-
-
+  
   /**
    * NGẮT KẾT NỐI ĐIỂM TRUY CẬP 
    * 
@@ -133,7 +151,7 @@ void setup()
    * - FALSE THÌ MODELE ESP8266 CHỈ NGỪNG PHÁT WIFI   * 
    */
   Serial.println("-----------------------------------------------------------------------------------");
-  Serial.print("NGẮT KẾT NỐI ĐIỂM TRUY CẬP \"");Serial.print(WiFi.softAPSSID());Serial.printf("\" %s\n",WiFi.softAPdisconnect(true) ? "THÀNH CÔNG" : "THẤT BẠI");
+  Serial.print("NGẮT KẾT NỐI ĐIỂM TRUY CẬP \"");Serial.print(WiFi.SSID());Serial.printf("\" %s\n",WiFi.disconnect(true) ? "THÀNH CÔNG" : "THẤT BẠI");
   Serial.println("-----------------------------------------------------------------------------------");
 }
 
